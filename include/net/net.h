588773996ecc16cec40ba437d876d7ed77a98e33
Yuri Benditovich <yuri.benditovich@daynix.com>
2020-10-23 12:52:44 +0300

    fix typo
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- include/net/net.h
+++ include/net/net.h
@@ -60,7 +60,7 @@ typedef int (SetVnetBE)(NetClientState *, bool);
 typedef struct SocketReadState SocketReadState;
 typedef void (SocketReadStateFinalize)(SocketReadState *rs);
 typedef void (NetAnnounce)(NetClientState *);
-typedef bool (SetStreeringEBPF)(NetClientState *, int);
+typedef bool (SetSteeringEBPF)(NetClientState *, int);
 
 typedef struct NetClientInfo {
     NetClientDriver type;
@@ -82,7 +82,7 @@ typedef struct NetClientInfo {
     SetVnetLE *set_vnet_le;
     SetVnetBE *set_vnet_be;
     NetAnnounce *announce;
-    SetStreeringEBPF *set_steering_ebpf;
+    SetSteeringEBPF *set_steering_ebpf;
 } NetClientInfo;
 
 struct NetClientState {
