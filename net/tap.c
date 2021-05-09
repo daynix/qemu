6ce9e9a3516ee82082aae3575e91eba579d17054
Yuri Benditovich <yuri.benditovich@daynix.com>
2021-05-09 14:30:25 +0300

    tap: implement method for USO feature probe
    Only Linux TUN/TAP device currently can support this feature,
    depending on support in kernel.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- net/tap.c
+++ net/tap.c
@@ -57,6 +57,7 @@ typedef struct TAPState {
     bool read_poll;
     bool write_poll;
     bool using_vnet_hdr;
+    bool has_uso;
     bool has_ufo;
     bool enabled;
     VHostNetState *vhost_net;
@@ -240,6 +241,15 @@ static bool tap_has_ufo(NetClientState *nc)
     return s->has_ufo;
 }
 
+static bool tap_has_uso(NetClientState *nc)
+{
+    TAPState *s = DO_UPCAST(TAPState, nc, nc);
+
+    assert(nc->info->type == NET_CLIENT_DRIVER_TAP);
+
+    return s->has_uso;
+}
+
 static bool tap_has_vnet_hdr(NetClientState *nc)
 {
     TAPState *s = DO_UPCAST(TAPState, nc, nc);
@@ -365,6 +375,7 @@ static NetClientInfo net_tap_info = {
     .poll = tap_poll,
     .cleanup = tap_cleanup,
     .has_ufo = tap_has_ufo,
+    .has_uso = tap_has_uso,
     .has_vnet_hdr = tap_has_vnet_hdr,
     .has_vnet_hdr_len = tap_has_vnet_hdr_len,
     .using_vnet_hdr = tap_using_vnet_hdr,
@@ -391,6 +402,7 @@ static TAPState *net_tap_fd_init(NetClientState *peer,
     s->host_vnet_hdr_len = vnet_hdr ? sizeof(struct virtio_net_hdr) : 0;
     s->using_vnet_hdr = false;
     s->has_ufo = tap_probe_has_ufo(s->fd);
+    s->has_uso = tap_probe_has_uso(s->fd);
     s->enabled = true;
     tap_set_offload(&s->nc, 0, 0, 0, 0, 0);
     /*
