bfec01aa31a06c6e0714cf20f39f4d55753cc0f7
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-01-15 14:36:30 +0900

    igbvf: Fix PCIDevice reference
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- hw/net/igbvf.c
+++ hw/net/igbvf.c
@@ -40,7 +40,7 @@
 #include "qemu/osdep.h"
 #include "hw/hw.h"
 #include "hw/net/mii.h"
-#include "hw/pci/pci.h"
+#include "hw/pci/pci_device.h"
 #include "hw/pci/pcie.h"
 #include "hw/pci/msix.h"
 #include "net/net.h"
