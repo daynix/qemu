275ea142e67c54bbaf39f663c7398af1f2b574f4
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-02-23 14:33:29 +0900

    e1000e: Implement system clock
    The system clock is necessary to implement PTP features. While we are
    not implementing PTP features for e1000e yet, we do have a plan to
    implement them for igb, a new network device derived from e1000e,
    so add system clock to the common base first.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- hw/core/machine.c
+++ hw/core/machine.c
@@ -41,7 +41,9 @@
 #include "hw/virtio/virtio-pci.h"
 #include "qom/object_interfaces.h"
 
-GlobalProperty hw_compat_7_2[] = {};
+GlobalProperty hw_compat_7_2[] = {
+    { "e1000e", "migrate-timadj", "off" },
+};
 const size_t hw_compat_7_2_len = G_N_ELEMENTS(hw_compat_7_2);
 
 GlobalProperty hw_compat_7_1[] = {
