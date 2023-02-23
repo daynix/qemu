b74826f544124ea2c2b718a414a58e297ee828aa
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-02-23 18:28:18 +0900

    e1000: Count CRC in Tx statistics
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- hw/net/e1000.c
+++ hw/net/e1000.c
@@ -636,7 +636,7 @@ xmit_seg(E1000State *s)
     }
 
     e1000x_inc_reg_if_not_full(s->mac_reg, TPT);
-    e1000x_grow_8reg_if_not_full(s->mac_reg, TOTL, s->tx.size);
+    e1000x_grow_8reg_if_not_full(s->mac_reg, TOTL, s->tx.size + 4);
     s->mac_reg[GPTC] = s->mac_reg[TPT];
     s->mac_reg[GOTCL] = s->mac_reg[TOTL];
     s->mac_reg[GOTCH] = s->mac_reg[TOTH];
