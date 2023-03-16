376fa5650762a9f887f250c655dd774ed0d1b3cb
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-03-16 13:58:26 +0900

    igb: Save the entire Tx context descriptor
    The current implementation of igb uses only part of a advanced Tx
    context descriptor because it misses some features and sniffs the trait
    of the packet instead of respecting the packet type specified in the
    descriptor. However, we will certainly need the entire Tx context
    descriptor when we update igb to respect these ignored fields. Save the
    entire Tx context descriptor to prepare for such a change.
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- hw/net/igb.c
+++ hw/net/igb.c
@@ -499,8 +499,10 @@ static const VMStateDescription igb_vmstate_tx = {
     .version_id = 1,
     .minimum_version_id = 1,
     .fields = (VMStateField[]) {
-        VMSTATE_UINT16(vlan, struct igb_tx),
-        VMSTATE_UINT16(mss, struct igb_tx),
+        VMSTATE_UINT32(ctx.vlan_macip_lens, struct igb_tx),
+        VMSTATE_UINT32(ctx.seqnum_seed, struct igb_tx),
+        VMSTATE_UINT32(ctx.type_tucmd_mlhl, struct igb_tx),
+        VMSTATE_UINT32(ctx.mss_l4len_idx, struct igb_tx),
         VMSTATE_BOOL(tse, struct igb_tx),
         VMSTATE_BOOL(ixsm, struct igb_tx),
         VMSTATE_BOOL(txsm, struct igb_tx),
