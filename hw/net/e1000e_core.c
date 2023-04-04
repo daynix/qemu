48070ba9c402bb4127d3d686521411ec897096ca
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-04-04 22:21:55 +0900

    e1000e: Fix iov length check
    The ethernet header starts at iov_ofs so the end of the ethernet header
    will be: iov_ofs + maximum_ethernet_hdr_len
    
    Fixes: 6f3fbe4ed0 ("net: Introduce e1000e device emulation")
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- hw/net/e1000e_core.c
+++ hw/net/e1000e_core.c
@@ -1695,7 +1695,7 @@ e1000e_receive_internal(E1000ECore *core, const struct iovec *iov, int iovcnt,
         iovcnt = 1;
         iov = &min_iov;
         iov_ofs = 0;
-    } else if (iov->iov_len < maximum_ethernet_hdr_len) {
+    } else if (iov->iov_len < iov_ofs + maximum_ethernet_hdr_len) {
         /* This is very unlikely, but may happen. */
         iov_to_buf(iov, iovcnt, iov_ofs, min_buf, maximum_ethernet_hdr_len);
         filter_buf = min_buf;
