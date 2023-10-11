1e4af17bef5bbf6972278cefadd411855ffc3bea
Akihiko Odaki <akihiko.odaki@daynix.com>
2023-10-11 23:42:14 +0900

    tap: Shrink zeroed virtio-net header
    tap prepends a zeroed virtio-net header when writing a packet to a
    tap with virtio-net header enabled but not in use. This only happens
    when s->host_vnet_hdr_len == sizeof(struct virtio_net_hdr).
    
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- net/tap.c
+++ net/tap.c
@@ -118,7 +118,7 @@ static ssize_t tap_receive_iov(NetClientState *nc, const struct iovec *iov,
     TAPState *s = DO_UPCAST(TAPState, nc, nc);
     const struct iovec *iovp = iov;
     struct iovec iov_copy[iovcnt + 1];
-    struct virtio_net_hdr_mrg_rxbuf hdr = { };
+    struct virtio_net_hdr hdr = { };
 
     if (s->host_vnet_hdr_len && !s->using_vnet_hdr) {
         iov_copy[0].iov_base = &hdr;
