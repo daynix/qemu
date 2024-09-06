97fe0a89062706d90526770bca494672a5162587
Akihiko Odaki <akihiko.odaki@daynix.com>
2024-09-06 21:42:49 +0900

    virtio-net: Do not check for the queue before RSS
    virtio_net_can_receive() checks if the queue is ready, but RSS will
    change the queue to use so, strictly speaking, we may still be able to
    receive the packet even if the queue initially provided is not ready.
    Perform RSS before virtio_net_can_receive() to cover such a case.
    
    Fixes: 4474e37a5b3a ("virtio-net: implement RX RSS processing")
    Signed-off-by: Akihiko Odaki <akihiko.odaki@daynix.com>

--- hw/net/virtio-net.c
+++ hw/net/virtio-net.c
@@ -1901,16 +1901,16 @@ static ssize_t virtio_net_receive_rcu(NetClientState *nc, const uint8_t *buf,
     size_t offset, i, guest_offset, j;
     ssize_t err;
 
-    if (!virtio_net_can_receive(nc)) {
-        return -1;
-    }
-
     if (!no_rss && n->rss_data.enabled && n->rss_data.enabled_software_rss) {
         int index = virtio_net_process_rss(nc, buf, size, &extra_hdr);
         if (index >= 0) {
             NetClientState *nc2 = qemu_get_subqueue(n->nic, index);
             return virtio_net_receive_rcu(nc2, buf, size, true);
         }
+    }
+
+    if (!virtio_net_can_receive(nc)) {
+        return -1;
     }
 
     /* hdr_len refers to the header we supply to the guest */
